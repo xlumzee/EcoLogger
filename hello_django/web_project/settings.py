e0bd52b297c851fbba2d568a35b1995bf3ef1ee9
Curlyheadedblackboy <humphrieszahir4@gmail.com>
2025-01-25 19:38:37 -0500

    " fix line 123, to be defined as a list"

--- hello_django/web_project/settings.py
+++ hello_django/web_project/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'web_project/static')
-}
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
