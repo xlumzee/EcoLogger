bfb5ba656a3fac9ac32a2cbf5160c65578a3dea9
Curlyheadedblackboy <humphrieszahir4@gmail.com>
2025-01-25 19:54:45 -0500

    "Move the web files around in their correct directories"

--- hello_django/web_project/settings.py
+++ hello_django/web_project/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "staticfiles",
 ]
 
 # Default primary key field type
